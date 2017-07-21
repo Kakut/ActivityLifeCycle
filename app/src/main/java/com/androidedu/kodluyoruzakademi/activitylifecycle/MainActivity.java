aa74c9ef4f92067db34f7d5d271d030a57f2dad1
Halil Kakut <halil.kakut@hotmail.com>
2017-07-22 01:22:16 +0300

    comment

--- app/src/main/java/com/androidedu/kodluyoruzakademi/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/androidedu/kodluyoruzakademi/activitylifecycle/MainActivity.java
@@ -42,12 +42,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         Log.e(TAG, "onRestoreInstanceState");
+
+        //onCreate metoduyla bilgileri yeniden yüklemek yerine onRestoreInstanceState() metoduyla durum
+        // bilgileri getirilebilir. Sistem onStart metodu sonrası otomatik
+        // olarak bu metodu çağırdığı için Bundle'ın null olup olmadığını kontrol etme zorunluluğu ortadan kalkmış olur.
+        //Fakat bu activity de yeniden bilgi yüklenme durumu yoktur. Bu metot çalışmaz.
     }
 
     @Override
     public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onPostCreate(savedInstanceState, persistentState);
         Log.e(TAG, "onPostCreate");
+        //onStart () ve onRestoreInstanceState (Bundle) çağrıldıktan sonra çağrılır.
+        //Bu Activity de onRestoreInstanceState (Bundle) çalışmadığından dolayı bu metot çalışmaz.
     }
 
     @Override
@@ -81,72 +88,99 @@ public class MainActivity extends AppCompatActivity {
         super.onPrepareOptionsMenu(menu);
         Log.e(TAG, "onPrepareOptionsMenu");
         return false;
+        //Varsayılan uygulama, sistem menüsü öğelerini etkinliğin durumuna göre güncelleştirir.
+        //Default sistem menü öğelerini tanımlar.
+        //Bu activity de menu tanımalaması kullanımı yoktur. Bu metot çalışmaz.
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.e(TAG, "onPause");
+        // Etkinliği düzenleyen herhangi bir ısrarcı durumu kaydetmek için, kullanıcıya bir "yerinde düzenleme"
+        // modeli sunmak ve yeni etkinliği ilk önce öldürmek için yeterli kaynak olmadığında hiçbir şeyin
+        // kaybolmadığından emin olmak için kullanılır. Bu Activity de öldürme olmadığı için metot çalışmaz.
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
         super.onSaveInstanceState(outState, outPersistentState);
         Log.e(TAG, "onSaveInstanceState");
+        //Bu yöntem, bir etkinliğin öldürülmesi öncesinde çağrılır;
+        // böylece gelecekte geri döndüğünde kaldığı durumuna geri dönebilir.
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.e(TAG, "onStop");
+        //Arka plana atıldığında çağırılır. Bu noktada iki seçenek vardır;
+        // ya kullanıcı yeni ekrandaki işini bitirip geri gelir ya da Activity tamamen kapatılır.
+        //Herhangi bir arka plana atılma yapılmadığı için metot çalışmaz.
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.e(TAG, "onDestroy");
+        //Activity'e ait bütün kaynaklar yok edilir. Activity bu noktada yaşam döngüsünü tamamlamıştır.
     }
 
     @Override
     public void onUserInteraction() {
         super.onUserInteraction();
         Log.e(TAG, "onUserInteraction");
+        //Etkinliklerin durum çubuğu bildirimlerini akıllıca yönetmesine yardımcı olmak için tasarlanmıştır; Özellikle,
+        // bir notasyonun iptal edilmesi için uygun zamanı belirleyen faaliyetlere yardımcı olmak için çağırılır.
     }
 
     @Override
     protected void onUserLeaveHint() {
         super.onUserLeaveHint();
         Log.e(TAG, "onUserLeaveHint");
+
+        //Bir etkinlik, kullanıcı seçimi sonucunda arka plana geçmek üzereyken
+        // etkinlik yaşam döngüsünün bir parçası olarak adlandırılır.
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Log.e(TAG, "onActivityResult");
+        //Bu çağrı, etkinliğiniz yeniden başladığında onResume () 'dan hemen önce alınır.
+        //Yeniden çağırılan bir Activity olmadığı için bu metot çalışmaz.
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.e(TAG, "onRestart");
+        //Kullanıcı geri geldiği takdirde onRestart metoduna geçilir ve döngü onStart metodundan tekrar başlar.
     }
 
     @Override
     public void onAttachFragment(Fragment fragment) {
         super.onAttachFragment(fragment);
         Log.e(TAG, "onAttachFragment");
+        //Activity bir fragment ile ilişkilendirildiğinde bu metot çağırılır. Bizim Activity miz içinde fragment yapısı
+        //kullanılmadığı için bu metot çalışmaz.
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         Log.e(TAG, "onConfigurationChanged");
+        //Bu işlev çağrıldığında, kaynaklar yeni yapılandırmayla
+        // eşleşen kaynak değerlerini döndürmek üzere güncellenecektir.
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         Log.e(TAG, "onBackPressed");
+        //Etkinlik kullanıcının geri tuşa basmasını tespit ettiğinde çağrılır.
+        // Varsayılan uygulama yalnızca geçerli etkinliği tamamlar, ancak bu istenildiği taktirde geçersiz kılınabilir.
     }
 
 }
